4801ad62e350628ef7ecf78b78a3ba61c58bc07c
Alsh <84447305+Alsh0815@users.noreply.github.com>
2024-05-13 15:00:53 +0900

    ver.0.1.1.25

--- app/src/main/java/com/x_viria/app/vita/somnificus/activity/SettingActivity.java
+++ app/src/main/java/com/x_viria/app/vita/somnificus/activity/SettingActivity.java
@@ -2,10 +2,14 @@ package com.x_viria.app.vita.somnificus.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.x_viria.app.vita.somnificus.R;
@@ -14,6 +18,12 @@ import com.x_viria.app.vita.somnificus.util.storage.SPKey;
 import com.x_viria.app.vita.somnificus.util.storage.SPStorage;
 
 public class SettingActivity extends AppCompatActivity {
+
+    private String getVersionName() throws PackageManager.NameNotFoundException {
+        String packageName = getPackageName();
+        PackageInfo packageInfo = getPackageManager().getPackageInfo(packageName, 0);
+        return packageInfo.versionName;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +43,20 @@ public class SettingActivity extends AppCompatActivity {
         TimerVibrate_L.setOnClickListener(v -> TimerVibrate_SW.setChecked(!TimerVibrate_SW.isChecked()));
         TimerVibrate_SW.setChecked(sps.getBool(SPKey.KEY__SETTINGS_TIMER_VIBRATE, SPDefault.SETTINGS_TIMER_VIBRATE));
         TimerVibrate_SW.setOnCheckedChangeListener((buttonView, isChecked) -> sps.setBool(SPKey.KEY__SETTINGS_TIMER_VIBRATE, isChecked));
+
+        LinearLayout Version_L = findViewById(R.id.SettingActivity__Version_L);
+        Version_L.setOnClickListener(v -> {
+            try {
+                Toast.makeText(getApplicationContext(), "ver." + getVersionName(), Toast.LENGTH_SHORT).show();
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        TextView Version_TX = findViewById(R.id.SettingActivity__Version_Text);
+        try {
+            Version_TX.setText(getVersionName());
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
